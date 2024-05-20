6a1083256487fd2bbf158af4a4ed612adf798915
Mazen <khaledmazen64@gmail.com>
2024-05-20 19:44:07 +0300

    cartt is done just add service and resource

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -2,20 +2,47 @@
 
 namespace App\Http\Controllers\Cart;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Cart\StoreCartRequest;
-use App\Http\Resources\Cart\CartResource;
 use App\Models\Cart;
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use App\Http\Service\Cart\CartService;
+use App\Http\Resources\Cart\CartResource;
+use App\Http\Requests\Cart\StoreCartRequest;
+use App\Http\Resources\Cart\StroeCartResource;
+
 
 
 class CartController extends Controller
 {
+
     public function index() 
     {   
         $user = auth()->user() ;
         $data = Cart::get() ;
         if($user) {
             return $this->apiResponse(CartResource::collection($data) , 'Cart retrieved successfully ' , 200 ) ;
+        }
+    }
+
+    public function store(StoreCartRequest $request , $id) 
+    {
+        $data               = $request->validated() ;
+        $user               = auth()->user() ;
+        $data['user_id']    = $user->id ;
+        $product            = Product::find($id) ;
+        $data['product_id'] = $product->id ;
+        $existingCartItem   = cart::where([
+            'user_id'    => $user->id,
+            'product_id' => $product->id
+        ])->first() ;
+        if($existingCartItem) 
+        {
+             return $this->apiResponse(null , 'this product is already in cart' , 400) ;
+        } 
+        $rec =   Cart::create($data) ;
+        if($rec) 
+        {
+            return $this->apiResponseStored($rec) ;
         }
     }
 
