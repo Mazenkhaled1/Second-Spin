2b5756b9549a58aa378fae3075b141c8fe489053
gannaadel1 <gannaadelll123@gmail.com>
2024-06-11 17:32:43 +0300

    images

--- app/Http/Service/Donations/MakeDonationService.php
+++ app/Http/Service/Donations/MakeDonationService.php
@@ -17,7 +17,8 @@ class MakeDonationService
            if($data) { 
              $imageName = str::random(32) . "." . $request->image->getClientOriginalExtension();
              Storage::disk('public')->put($imageName, file_get_contents($request->image));
-             $data['image'] = $imageName ;
+             $filePath =url('/public/storage/'.$imageName);
+         $data['image']   =$filePath ;
              $data['user_id'] = $request->user()->id;
              $charity=Charity::find($id);
              $data['charity_id'] = $charity->id;
--- app/Http/Service/Products/AllProductsService.php
+++ app/Http/Service/Products/AllProductsService.php
@@ -24,7 +24,8 @@ class AllProductsService
         $data['category_id'] = $category->id ; 
         $imageName = str::random(32) . "." . $request->image->getClientOriginalExtension();
         Storage::disk('public')->put($imageName, file_get_contents($request->image));
-        $data['image']   = $imageName ;
+         $filePath =url('/public/storage/'.$imageName);
+         $data['image']   =$filePath ;
         $data['price'] = $request->price + ($request->price * 0.12) ;
         $insertProduct = Product::create($data) ;
         return $insertProduct  ; 
