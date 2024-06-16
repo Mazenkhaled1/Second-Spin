f455963fa2d6ed70f69b443a4d742370e5220342
gannaadel1 <gannaadelll123@gmail.com>
2024-06-16 21:48:24 +0300

    admin updates

--- resources/views/Admin/inc/footer.blade.php
+++ resources/views/Admin/inc/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer">
+{{-- <footer class="main-footer">
   <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
@@ -11,4 +11,4 @@
   <!-- Control sidebar content goes here -->
 </aside>
 <!-- /.control-sidebar -->
-</div>
+</div> --}}
